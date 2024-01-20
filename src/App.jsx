d74e257a004d7bc9093900e55cb3bf591d777606
arifwp <arifwahyu696@gmail.com>
2024-01-20 15:13:45 +0700

    working with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { CORE_CONCEPTS } from './data';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('jsx');
+
+  function handleClicked(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -22,6 +31,24 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton clicked={() => handleClicked('components')}>Components</TabButton>
+            <TabButton clicked={() => handleClicked('jsx')}>JSX</TabButton>
+            <TabButton clicked={() => handleClicked('props')}>Props</TabButton>
+            <TabButton clicked={() => handleClicked('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton({ children, clicked }) {
+    return <li>
+        <button onClick={clicked}>
+            {children}
+        </button>
+    </li>
+}
