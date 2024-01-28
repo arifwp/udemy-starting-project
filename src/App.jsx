c3fa3c39e3670b2f6126747fbb2111112e017501
arifwp <arifwahyu696@gmail.com>
2024-01-28 21:33:07 +0700

    learn creating flexible components

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,16 @@
-import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { useState } from 'react';
-import { EXAMPLES } from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleClicked(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic</p>;
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id='core-concepts'>
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === 'components'}
-              clicked={() => handleClicked('components')}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'jsx'}
-              clicked={() => handleClicked('jsx')}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'props'}
-              clicked={() => handleClicked('props')}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'state'}
-              clicked={() => handleClicked('state')}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,6 @@
-export default function TabButton({ children, clicked, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
     return <li>
-        <button className={isSelected ? 'active' : undefined} onClick={clicked}>
+        <button className={isSelected ? 'active' : undefined} {...props}>
             {children}
         </button>
     </li>
